bca462b4b75dcc9d912b05d92d6ba88a6e02662b
Dimas Sul Ulum <dimassns16@gmail.com>
2022-11-08 19:40:53 +0700

    fixing bug searching artikel

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -29,22 +29,23 @@
         </div>
     </section>
     <!-- end of hero slider -->
-    <script>
-        let start = 0;
-        otomatis();
-
-        function otomatis() {
-            const slide = document.querySelectorAll(".slide");
-            const slick = document.querySelectorAll(".slick-next")
-
-
-            slide[start].style.display = "block";
-            start++;
-
-            setTimeout(slick, 2000);
-
-        }
-    </script>
+    {{-- <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            @foreach ($slider as $slider)
+                <div class="carousel-item" data-bs-interval="2000">
+                    <img src="{{ asset('assets/') }}../../{{ $slider->gambar }}" class="d-block w-100" alt="...">
+                </div>
+            @endforeach
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div> --}}
     <!-- about-area start -->
 
     <div class="wpo-about-area-3 section-padding">
